4c15cfc4089264c421b0e83a0082e9e7abb9262b
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-10 21:36:50 +0200

    create text information

---
+++ src/components/TextInformation.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>{{title}}</h2>
+        <p>{{body}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'TextInformation',
+  props: {
+    title: String,
+    body: String,
+  },
+})
+export default class TextInformation extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
