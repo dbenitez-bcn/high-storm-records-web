0f3094a05afe832ae89cc28dffaefcfd9b668591
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-13 14:23:06 +0200

    adds copyright component

---
+++ src/components/Copyright.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center">
+      {{ new Date().getFullYear() }} — <strong>High Storm Records&#174;</strong>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Copyright',
+})
+export default class Copyright extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
