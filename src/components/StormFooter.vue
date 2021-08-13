fdf03963158e7607b65545652c04632abb13f899
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-13 14:23:32 +0200

    updates footer

--- src/components/StormFooter.vue
+++ src/components/StormFooter.vue
@@ -1,22 +1,32 @@
 <template>
-  <v-footer class="black white--text">
-    <social-media />
-    <v-col class="text-center" cols="12">
-      {{ new Date().getFullYear() }} — <strong>High Storm Records&#174;</strong>
-    </v-col>
-    <contact />
+  <v-footer padless class="black white--text">
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="4">
+          <social-media />
+        </v-col>
+        <v-col class="order-sm-last" cols="12" sm="4">
+          <contact class="order-sm-last" />
+        </v-col>
+        <v-col cols="12" sm="4" align-self="center">
+          <copyright />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import SocialMedia from '@/components/SocialMedia.vue';
+import Copyright from '@/components/Copyright.vue';
 import Contact from '@/components/Contact.vue';
 
 export default Vue.extend({
   name: 'StormFooter',
   components: {
     SocialMedia,
+    Copyright,
     Contact,
   },
 });
