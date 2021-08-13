02f6678300fbb94cc7f32df6a1945dba0292e90d
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-13 15:23:38 +0200

    create buttons

---
+++ src/components/Buttons.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <div class="d-flex flex-column">
+          <v-btn class="white--text" color="#C13584">
+            <a
+              href="https://www.instagram.com/highstormrecords/"
+              target="_blank"
+            >
+              Contactanos por instagram
+            </a>
+          </v-btn>
+          <div class="d-flex justify-center align-center">
+            <div class="storm-divider black"></div>
+            <div class="px-2">o</div>
+            <div class="storm-divider black"></div>
+          </div>
+          <v-btn class="white--text" color="black"
+            >highstormrecords@gmail.com</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Buttons extends Vue {}
+</script>
+
+<style scoped>
+.storm-divider {
+  width: 100%;
+  height: 1px;
+}
+a:link {
+  text-decoration: inherit;
+  color: inherit;
+  cursor: pointer;
+}
+
+a:visited {
+  text-decoration: inherit;
+  color: inherit;
+  cursor: pointer;
+}
+</style>
