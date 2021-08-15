716817a952c3ef189b5b22069369e25ff5823920
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-15 03:36:19 +0200

    improves css generation in storm header

--- src/components/StormHeader.vue
+++ src/components/StormHeader.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="storm-header" :class="getClassBasedOnBreakpoint">
-    </div>
+  <div :class="getClassBasedOnBreakpoint">
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,36 +20,46 @@ export default class StormHeader extends Vue {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .storm-header {
-    width: 100%;
-    background-image: url('../assets/img/headerBg.jpg');
-    background-repeat: no-repeat;
-    clip-path: polygon(0 0, 100% 0, 100% 82%, 0 100%);
-}
-.storm-header-xs {
+  width: 100%;
+  background-image: url("../assets/img/headerBg.jpg");
+  background-repeat: no-repeat;
+  clip-path: polygon(0 0, 100% 0, 100% 82%, 0 100%);
+
+  &-xs {
+    @extend .storm-header;
     height: 180px;
     background-size: 122%;
     background-position: 6% 33%;
-}
-.storm-header-sm {
+  }
+
+  &-sm {
+    @extend .storm-header;
     height: 256px;
     background-size: 122%;
     background-position: 6% 33%;
-}
-.storm-header-md {
+  }
+
+  &-md {
+    @extend .storm-header;
     height: 50vh;
     background-size: 142%;
     background-position: 30% 33%;
-}
-.storm-header-lg {
+  }
+
+  &-lg {
+    @extend .storm-header;
     height: 90vh;
     background-size: 110%;
     background-position: 6% 33%;
-}
-.storm-header-xl {
+  }
+
+  &-xl {
+    @extend .storm-header;
     height: 90vh;
     background-size: 110%;
     background-position: 6% 33%;
+  }
 }
 </style>
