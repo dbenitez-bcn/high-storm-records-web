6ce31dc359956f6d59b059540304724e2442ce0a
David Benitez Riba <dbr.bcn@gmail.com>
2021-08-15 03:18:59 +0200

    adds header viewports and shape

--- src/components/StormHeader.vue
+++ src/components/StormHeader.vue
@@ -1,31 +1,55 @@
 <template>
-    <div class="storm-header">
-        <!-- <div class="triangle white"></div> -->
+    <div class="storm-header" :class="getClassBasedOnBreakpoint">
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Component from 'vue-class-component';
 
-export default Vue.extend({
+@Component({
   name: 'StormHeader',
-});
+  props: {
+    image: String,
+  },
+})
+export default class StormHeader extends Vue {
+  get getClassBasedOnBreakpoint(): string {
+    return `storm-header-${this.$vuetify.breakpoint.name}`;
+  }
+}
 </script>
 
 <style scoped>
 .storm-header {
     width: 100%;
-    height: 200px;
     background-image: url('../assets/img/headerBg.jpg');
     background-repeat: no-repeat;
-    background-size: 122%;
-    background-position: 6% 28%;
+    clip-path: polygon(0 0, 100% 0, 100% 82%, 0 100%);
 }
-.triangle {
-width: 0;
-height: 0;
-border-style: solid;
-border-width: 0 0 316px 330px;
-border-color: transparent transparent #007bff transparent;
+.storm-header-xs {
+    height: 180px;
+    background-size: 122%;
+    background-position: 6% 33%;
+}
+.storm-header-sm {
+    height: 256px;
+    background-size: 122%;
+    background-position: 6% 33%;
+}
+.storm-header-md {
+    height: 50vh;
+    background-size: 142%;
+    background-position: 30% 33%;
+}
+.storm-header-lg {
+    height: 90vh;
+    background-size: 110%;
+    background-position: 6% 33%;
+}
+.storm-header-xl {
+    height: 90vh;
+    background-size: 110%;
+    background-position: 6% 33%;
 }
 </style>
